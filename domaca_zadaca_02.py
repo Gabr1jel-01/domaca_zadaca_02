5798c1281458931e8296515dc277aeb873c7f2d4
Gabrijel <g.gordic@outlook.com>
2023-02-18 20:00:29 +0100

    Domaca zadaca
    drugi zadatak

---
+++ domaca_zadaca_02.py
@@ -0,0 +1,21 @@
+# kreirati jednostavan kalkulator
+# osigurati osnovne racunske operacije
+# zbrajanje, oduzimanje, mnozenje, dijeljenje cjelobrojno i modulo
+# kreirajte nekoliko varijabli sa brojevima te ispisite rezultate racunskih operacija 
+
+
+a = 10
+b = 20
+c = 30
+d = 2
+e = 3
+f = 7
+
+zbrajanje = a + b
+oduzimanje = c - f
+mnozenje = d * b
+dijeljenje_cjelobrojno = c / d
+dijeljenje_modulo = f % e
+
+print(zbrajanje, oduzimanje, mnozenje, dijeljenje_cjelobrojno, dijeljenje_modulo, sep="\n")
+
